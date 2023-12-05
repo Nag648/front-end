65e2f140f5af9d9f23e75769e3970ed1c5c8bf8b
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-12-05 07:48:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [newTodo,setNewTodo] = useState('');
 
+
+  const addTodo = async () => {
+    const data = {
+      text: newTodo,
+      completed: false
+    }
+    try {
+      const response = await axios.post('http://localhost:5000/Todo',data);
+      // fetchTodos()
+      // setTodos([...todos, response.data]);
+      setNewTodo('');
+    }
+    catch (error){
+      console.error(error);
+    }
+
+  };
+
+  return(
+    <div>
+      <h1>Todi App</h1>
+      <ul>
+        {todos.map(todo =>(
+          <li key={todo._id}>{todo.text}</li>
+        ))}
+      </ul>
+      <div>
+        <input type='text' value={newTodo} onChange={(e)=> {setNewTodo(e.target.value)}}/>
+
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    </div>
+  )
+
+
+}
 export default App;
